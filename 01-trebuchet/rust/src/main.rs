92caae1610cf9c5bbe2137302537a027430a460e
Mateus Sampaio <mateus4k@protonmail.ch>
2023-12-19 12:47:00 -0300

    Add day 1 part 2

--- 01-trebuchet/rust/src/main.rs
+++ 01-trebuchet/rust/src/main.rs
@@ -3,14 +3,20 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("../input.txt").expect("error while reading file");
 
-    let result: i32 = input
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
+}
+
+fn part_1(input: &String) -> u32 {
+    let result: u32 = input
+        .trim()
         .split('\n')
         .filter_map(|line| {
             let mut first = String::new();
             let mut last = String::new();
 
             line.chars().for_each(|char| {
-                if char.to_string().parse::<i32>().is_err() {
+                if char.to_string().parse::<u32>().is_err() {
                     return;
                 }
 
@@ -21,9 +27,39 @@ fn main() {
                 }
             });
 
-            format!("{first}{last}").parse::<i32>().ok()
+            format!("{first}{last}").parse::<u32>().ok()
         })
-        .sum::<i32>();
+        .sum::<u32>();
 
-    println!("Result: {}", result);
+    result
+}
+
+fn part_2(input: &String) -> u32 {
+    let result = input
+        .trim()
+        .split('\n')
+        .map(|line| {
+            let mut iter = (0..line.len()).filter_map(|index| match &line[index..] {
+                line if line.starts_with("one") => Some(1),
+                line if line.starts_with("two") => Some(2),
+                line if line.starts_with("three") => Some(3),
+                line if line.starts_with("four") => Some(4),
+                line if line.starts_with("five") => Some(5),
+                line if line.starts_with("six") => Some(6),
+                line if line.starts_with("seven") => Some(7),
+                line if line.starts_with("eight") => Some(8),
+                line if line.starts_with("nine") => Some(9),
+                line => line.chars().next().unwrap().to_digit(10),
+            });
+
+            let first = iter.next().expect("should be a number");
+
+            match iter.last() {
+                Some(num) => first * 10 + num,
+                None => first * 10 + first,
+            }
+        })
+        .sum::<u32>();
+
+    result
 }
