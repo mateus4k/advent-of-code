487bf377119dac66f92549bf7d190a0e395c538b
Mateus Sampaio <mateus4k@protonmail.ch>
2024-12-05 18:00:50 -0300

    feat: add day 5 files

---
+++ 2024/05-print-queue/index.mjs
@@ -0,0 +1,21 @@
+import { readFileSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+
+const currentDir = dirname(new URL(import.meta.url).pathname);
+
+function main() {
+    const input = readFileSync(join(currentDir, './test.txt')).toString();
+
+    console.log('Part 1: ', part1(input));
+    console.log('Part 2: ', part2(input));
+}
+
+function part1(input) {
+    return;
+}
+
+function part2(input) {
+    return;
+}
+
+main();
