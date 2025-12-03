b6f0ea6b4a5dd29213144443beae6aedc8743010
Mateus Sampaio <mateus4k@protonmail.ch>
2025-12-03 00:13:13 -0300

    add day 1 part 2

--- 2025/src/01_secret_entrance/__init__.py
+++ 2025/src/01_secret_entrance/__init__.py
@@ -6,9 +6,9 @@ def phase_1(data=None):
     acc = 0
 
     for line in data:
-        next = line.strip()
-        direction = next[0]
-        value = int(next[1:])
+        line = line.strip()
+        direction = line[0]
+        value = int(line[1:])
 
         if direction == "L":
             current = (current - value) % 100
@@ -19,10 +19,36 @@ def phase_1(data=None):
         if current == 0:
             acc += 1 
 
-    print(current, acc)
+    print(acc)
 
 def phase_2(data=None):
-    print(data)
+    current = 50
+    acc = 0
+
+    for line in data:
+        line = line.strip()
+        direction = line[0]
+        value = int(line[1:])
+        start = current
+        
+        full_loops = value // 100
+        rest = value % 100
+
+        passes = full_loops
+
+        if direction == "R":
+            if start + rest >= 100:
+                passes += 1
+            current = (start + value) % 100
+
+        elif direction == "L":
+            if start > 0 and start - rest <= 0:
+                passes += 1
+            current = (start - value) % 100
+
+        acc += passes
+
+    print(acc)
 
 def main():
     base = Path(__file__).resolve().parent
@@ -30,4 +56,4 @@ def main():
     data = import_txt_as_list(file_path)
 
     phase_1(data)
-    #phase_2(data)
+    phase_2(data)
